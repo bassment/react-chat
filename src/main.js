c0d4df81234a354da92b565306cde69ac31c0c79
codterpin <anton.perebyinis@gmail.com>
2016-01-12 12:29:51 +0200

    Change to browserHistory instead of hashHistory

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from 'react-router';
-import createHashHistory from 'history/lib/createHashHistory';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from './routes/index.jsx';
 
 ReactDOM.render(<Router
     children={routes}
-    history={createHashHistory({ queryKey: false })}/>,
+    history={createBrowserHistory({ queryKey: false })}/>,
   document.getElementById('container'));
